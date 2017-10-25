2c295c9fbba2cbca69d247e80e0a4d7f4cfd1400
Chad Yates <cayates1989@gmail.com>
2017-10-25 15:17:55 -0400

    state is correctly changing to show code for basketball section when the button is clicked

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -24,9 +24,9 @@ import {
 } from 'react-viro';
 
 import SmokeEmitter from './components/SmokeEmitter'
-import MLScene from './components/MLScene'
+// import MLScene from './components/MLScene'
 import CYScene from './components/CYScene'
-import BAScene from './components/BAScene'
+// import BAScene from './components/BAScene'
 
 var HelloWorldSceneAR = React.createClass({
   getInitialState() {
@@ -59,11 +59,11 @@ var HelloWorldSceneAR = React.createClass({
          /> */}
 
       
-        <MLScene />
+        {/*<MLScene />*/}
 
         <CYScene />
 
-        <BAScene />
+        {/*<BAScene />*/}
 
       </ViroARScene>
     );
