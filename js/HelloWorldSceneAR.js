8a2be4dda0ec8a323dcc1a6b43efec58de0de9ae
Brandon Aiton <brandonaiton@icloud.com>
2017-10-25 16:30:28 -0400

    created a button to access a form through react native

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Button, View} from 'react-native';
 
 import {
   ViroARScene,
@@ -24,79 +24,36 @@ import {
 } from 'react-viro';
 
 import SmokeEmitter from './components/SmokeEmitter'
-// import MLScene from './components/MLScene'
-import CYScene from './components/CYScene'
-// import BAScene from './components/BAScene'
+import MLScene from './components/MLScene'
+// import CYScene from './components/CYScene'
+import BAScene from './components/BAScene'
 
-var HelloWorldSceneAR = React.createClass({
-  getInitialState() {
-    return {
-      text : "Initializing AR..."
-    };
-  },
+class HelloWorldSceneAR extends Component{
+constructor(){
+  super()
+  this.state= {
+    formVis: false
+  }
+}
 
-  render: function() {
+  // _handlePress = (event) =>{
+  //   this.setState({formVis: true})
+  // }
+
+  render() {
     return (
       <ViroARScene onTrackingInitialized={()=>{this.setState({text : "hiAR.me"})}}>
-      <ViroAmbientLight color="#ffffff" intensity={200}/>
-        {/* <ViroText
-          text={this.state.text}
-          scale={[.5, .5, .5]}
-          position={[0, .5, -1]}
-          style={styles.helloWorldTextStyle}
-          />
-        <ViroVideo
-            source={require('./res/hire-me.mp4')}
-            onClick={this._onClick}
-            loop={true}
-            position={[0,0,-1]}
-            scale={[.25, .25, 0]}
-            animation={{name: 'growAndShrink',
-            run: true,
-            loop: true,
-            delay: 0  //delay before starting loop
-            }}
-         /> */}
-
+        <ViroAmbientLight color="#ffffff" intensity={200}/>
         <MLScene />
-
-
-        <CYScene />
-
+        {/* <CYScene /> */}
         <BAScene />
-
       </ViroARScene>
-    );
-  },
+    )
+  }
 
   _onClick(source) {
       this.setState({text: "Clicked"});
-    },
+    }
 
-});
-//
-// ViroAnimations.registerAnimations({
-//   // define the name
-//   grow: {properties:{scaleX:1.0, scaleY:1.0, scaleZ:0,
-//                             opacity: 1.0},
-//                 easing:"Bounce",
-//                 duration: 1000},
-//   rotate:{properties:{rotateZ:"+=45"}, duration:1000},
-//   shrink: {properties:{scaleX:0.5, scaleY:0.5, scaleZ:0,
-//                             opacity: 1.0},
-//                 easing:"Bounce",
-//                 duration: 1000},
-//   growAndShrink: [["grow", "shrink"]]  //array of animated movements
-// });
-//
-// var styles = StyleSheet.create({
-//   helloWorldTextStyle: {
-//   fontFamily: 'Arial',
-//   fontSize: 30,
-//   color: '#ffffff',
-//   textAlignVertical: 'center',
-//   textAlign: 'center',
-//   },
-// });
-
+};
 module.exports = HelloWorldSceneAR;
